ff61cdb6580dddb1678e7deca4abe3bb97eae84b
Guilherme Casassanta <guilherme.casassanta@gmail.com>
2022-02-20 18:13:14 -0300

    Task 4 updated

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-import PracticeAlgorithms.*
+import PracticeAlgorithms.diagonalDifferences
 
 fun main(args: Array<String>) {
 
@@ -10,8 +10,8 @@ fun main(args: Array<String>) {
     // val result3 = compareTriplets(arrayOf(5,6,7), arrayOf(3,6,10))  //#3
     // println("Alice = ${result3[0]} - Bob = ${result3[1]}")          //#3
 
-    // println(AVeryBigSum(arrayOf(1000000001, 1000000002, 1000000003, 1000000004, 1000000005)))  //#4
+    // println(aVeryBigSum(arrayOf(1000000001, 1000000002, 1000000003, 1000000004, 1000000005)))  //#4
 
-    // println(DiagonalDifferences(arrayOf(arrayOf(11,2,4), arrayOf(4,5,6), arrayOf(10,8,-12))))  //#5
+    println(diagonalDifferences(arrayOf(arrayOf(11,2,4), arrayOf(4,5,6), arrayOf(10,8,-12))))  //#5
 
 }
